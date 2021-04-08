d12126cbbbe47d9ed3cbe05289a9b73eb61ae318
zhangbo <12345678>
2021-04-08 15:50:19 +0800

    修改

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,14 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
+    <zhb-button>按钮</zhb-button>
+    <zhb-button type="primary">primary按钮</zhb-button>
+    <zhb-button type="success">success按钮</zhb-button>
+    <zhb-button type="info">info按钮</zhb-button>
+    <zhb-button type="danger">danger按钮</zhb-button>
+    <zhb-button type="warning">warning按钮</zhb-button>
   </div>
 </template>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
 
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
 </style>
--- src/main.js
+++ src/main.js
@@ -2,9 +2,11 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+import ZhbUi from 'zhb-ui'
+import 'zhb-ui/dist/zhb-ui.css'
 
 Vue.config.productionTip = false
-
+Vue.use(ZhbUi)
 new Vue({
   router,
   store,
