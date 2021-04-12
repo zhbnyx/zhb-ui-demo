104b1522bd4ec34aab3c24f4f6b0cf4db9b285f3
zhangbo <12345678>
2021-04-12 16:13:49 +0800

    更新运用zhb-ui

--- src/App.vue
+++ src/App.vue
@@ -6,9 +6,32 @@
     <zhb-button type="info">info按钮</zhb-button>
     <zhb-button type="danger">danger按钮</zhb-button>
     <zhb-button type="warning">warning按钮</zhb-button>
+
+    <h6>ZhbCheckGroup</h6>
+    <zhb-check-group v-model="checkGroup"  @change="changeCheckGroup" >
+      <ZhbCheck label="0">输电线</ZhbCheck>
+      <ZhbCheck label="1">变电站</ZhbCheck>
+    </zhb-check-group>
+
+
+
   </div>
 </template>
+<script>
+  export default {
+    data(){
+      return {
+       checkGroup:[]
+      }
+    },
+    methods: {
 
+      changeCheckGroup(){
+        console.log(this.checkGroup);
+      }
+    }
+  }
+</script>
 <style lang="scss">
 
 </style>
